64d19e125f6a383c3b4d7d834f31f7eb3877bcc7
shaunpersad <shaunpersad@gmail.com>
2016-10-18 04:01:59 -0400

    adjusting styles

---
+++ universe.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Launch Chromium in Kiosk with specified URL
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --kiosk --app=https://mission-control-demo.herokuapp.com/universe.html
